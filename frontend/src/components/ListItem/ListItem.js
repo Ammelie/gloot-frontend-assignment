52bf087519f5c3659ae93ccafd186cc08c240cbb
Ammelie <ammelieneth@gmail.com>
2019-09-22 20:48:08 +0200

    List item is now a form

--- frontend/src/components/ListItem/ListItem.js
+++ frontend/src/components/ListItem/ListItem.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./ListItem.css";
+import Form from "../Form/Form";
 
 function ListItem(props) {
-  return <li key={`${props.player.id}`}>{props.player.name}</li>;
+  return (
+    <li>
+      <Form
+        buttonText="Rename"
+        placeholder={props.player.name}
+        player={props.player}
+        styling={"edit-player-form"}
+        submit={props.updatePlayer}
+        type="UPDATE"
+        value={props.player.name}
+      ></Form>
+    </li>
+  );
 }
 
 export default ListItem;
